a5d765a9c81146d2a3241b32f479b7121e93b09d
Albina Gilmutdinova <inovaras@mail.ru>
2023-01-01 19:36:24 +0500

    fill class InfoMessage and class Training

--- homework.py
+++ homework.py
@@ -1,25 +1,53 @@
+M_IN_KM: int = 1000
+
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
+    def __init__(self,
+                 training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: float
+                 ) -> None:
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+
+    def show_training_info(self):
+        training_types: dict = {'SWM': 'Swimming', 'RUN': 'Running', 'WLK': 'SportsWalking'}
+        return training_types[self.training_type]()
+
+    def get_message(self):
+        return (f'Тип тренировки: {self.training_type};'
+                f' Длительность: {round(self.duration, 3)} ч.;'
+                f' Дистанция: {round(self.distance, 3)} км;'
+                f' Ср. скорость: {round(self.speed, 3)} км/ч;'
+                f' Потрачено ккал: {round(self.calories, 3)}.')
 
 
 class Training:
     """Базовый класс тренировки."""
-
+    LEN_STEP = 0.65
+    
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
                  ) -> None:
-        pass
+        self.action = action
+        self.duration = duration
+        self.weight = weight
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        pass
+        return self.action * self.LEN_STEP / M_IN_KM
 
-    def get_mean_speed(self) -> float:
+    def get_mean_speed(self, training_time: int) -> float:
         """Получить среднюю скорость движения."""
-        pass
+        distance = self.get_distance()
+        return distance / training_time
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -27,7 +55,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
+        return InfoMessage()
 
 
 class Running(Training):
